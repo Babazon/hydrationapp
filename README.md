d8fb464be37d1b3a21ec83b62778431eccd52f05
Babazon <basar.yuksel@mantro.net>
2019-12-14 17:34:27 +0100

    added another possible fix to prevent appcenter crash

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Initial commit
+[![Build status](https://build.appcenter.ms/v0.1/apps/2b055295-8241-4599-9ed4-1f4bd2121e3b/branches/app-center-setup/badge)](https://appcenter.ms)
--- jest-setup.js
+++ jest-setup.js
@@ -4,7 +4,6 @@ require('react-native');
 require('jest-enzyme');
 
 const httpAdapter = require('axios/lib/adapters/http');
-const axios = require('axios');
 const Enzyme = require('enzyme');
 const Adapter = require('enzyme-adapter-react-16');
 
