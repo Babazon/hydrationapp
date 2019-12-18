11d84974fc9879b80b09f1ac4638a50a05ab9aa1
Babazon <basar.yuksel@mantro.net>
2019-12-18 18:51:52 +0100

    added test to pre commit hook

--- jest-setup.js
+++ jest-setup.js
@@ -3,12 +3,8 @@ require('reflect-metadata');
 require('react-native');
 require('jest-enzyme');
 
-const httpAdapter = require('axios/lib/adapters/http');
 const Enzyme = require('enzyme');
 const Adapter = require('enzyme-adapter-react-16');
 
-global.FormData = require('form-data');
-
-axios.defaults.adapter = httpAdapter;
 
 Enzyme.configure({adapter: new Adapter()});
