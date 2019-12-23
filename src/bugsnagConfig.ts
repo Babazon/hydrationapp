2f8ed7f62e3ba95d9bf3f39bf1b055c1faa5e9e2
Babazon <basar.yuksel@mantro.net>
2019-12-23 17:25:26 +0100

    added bugsnag integration with code, and github

---
+++ src/bugsnagConfig.ts
@@ -0,0 +1,14 @@
+import { Client, Configuration } from 'bugsnag-react-native';
+
+const config = new Configuration();
+
+config.automaticallyCollectBreadcrumbs = true;
+config.apiKey = 'aff0984eb5e47164b2bf18916afdbf67';
+config.autoCaptureSessions = true;
+config.consoleBreadcrumbsEnabled = true;
+config.handlePromiseRejections = true;
+config.autoNotify = true;
+
+export const bugsnag = new Client(config);
+
+bugsnag.notify(new Error('test'));
